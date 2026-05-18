e9c69d3f0c4e310ba891d3162fd743576ebe0347
jto05 <julian.to2023@gmail.com>
2026-05-18 00:01:49 -0700

    (feat) implemented Hub and relevant channels

--- server/chat.go
+++ server/chat.go
@@ -1,22 +1,77 @@
+/*
+server/chat.go
+
+A pub-sub router that broadcasts messaeges between subscribers
+*/
+
 package main
 
-import "sync"
-
+/*
+Message
+A message contains its contents and an associated Username
+*/
 type Message struct {
 	Username string `json:"username"`
 	Content  string `json:"content"`
 }
 
+/*
+Hub
+The hub that tracks a list of clients, a channel for registering clients,
+a channel of for unregistering clients, and a channel for broadcasting;
+uses a "chan Message" as a unique identifier for a client. Clients
+is a map for easier deletion
+*/
 type Hub struct {
-	mu sync.RWMutex
+	clients    map[chan Message]bool
+	register   chan chan Message
+	unregister chan chan Message
+	broadcast  chan Message
 }
 
+/*
+NewHub()
+Allocates space for a new Hub and returns the struct.
+*/
 func NewHub() *Hub {
-	return &Hub{}
+	return &Hub{
+		clients:    make(map[chan Message]bool),
+		register:   make(chan chan Message),
+		unregister: make(chan chan Message),
+		broadcast:  make(chan Message),
+	}
 }
 
+/*
+Run()
+A loop that intercepts messages in each channel to select from
+three actions: register, unregister, broadcast.
+*/
 func (h *Hub) Run() {
+	for {
+		select {
+		// register new user and add them to clients list
+		case client := <-h.register:
+			h.clients[client] = true
+
+		// unregister new user by removing them from clients list
+		case client := <-h.unregister:
+			delete(h.clients, client)
+			close(client)
+
+		// broadcast messages to all clients
+		case msg := <-h.broadcast:
+			for client := range h.clients {
+				client <- msg
+			}
+		}
+	}
 }
 
+/*
+Broadcast
+Sends message to hub's broadcast channel
+*/
 func (h *Hub) Broadcast(msg Message) {
+	h.broadcast <- msg
 }
